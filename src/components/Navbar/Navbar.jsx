14096a5e8a8e062e00a31ce3ae5129b397286dec
jskodova <skodovajarmilatt@gmail.com>
2022-11-13 01:52:36 +0200

    added css to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,9 +18,19 @@ const Navbar = () => (
       <li className='link__opensans'><a href='#contact'>Contact</a></li>
     </ul>
     <div className='site__navbar-login'>
-    <a href="login" className='link__opensans'>Log in / Register</a>
-    <div></div>
-    <a href="/" className='link__opensans'>Book a table</a>
+      <a href="login" className='link__opensans'>Log in / Register</a>
+      <div></div>
+      <a href="/" className='link__opensans'>Book a table</a>
+    </div>
+    <div className='site__hiddentoggle'>
+    <GiHamburgerMenu color='white' fontSize={27} onClick={() => ()}/>
+    <ul className='site__navbar__toggle-links'>
+      <li className='link__opensans'><a href='#home'>Home</a></li>
+      <li className='link__opensans'><a href='#about'>About</a></li>
+      <li className='link__opensans'><a href='#menu'>Menu</a></li>
+      <li className='link__opensans'><a href='#awards'>Awards</a></li>
+      <li className='link__opensans'><a href='#contact'>Contact</a></li>
+    </ul>
     </div>
   </nav>
 );
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,6 +1,8 @@
 import bg from '../assets/bg.png';
 import chef from '../assets/chef.png';
 import G from '../assets/G.png';
+import boreas from "../assets/boreas.png";
+import boreaslogo from "../assets/Boreas-1.png";
 import gallery01 from '../assets/gallery01.png';
 import gallery02 from '../assets/gallery02.png';
 import gallery03 from '../assets/gallery03.png';
@@ -25,6 +27,8 @@ export default {
   bg,
   chef,
   G,
+  boreas,
+  boreaslogo,
   gallery01,
   gallery02,
   gallery03,
