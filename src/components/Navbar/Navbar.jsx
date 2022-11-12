7a0eb9df8140b1231dc655ef0d222374b686e90c
jskodova <skodovajarmilatt@gmail.com>
2022-11-12 23:42:26 +0200

    updated navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 
 import './Navbar.css';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import images from "../../constants/images";
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className='site__navbar'>
+    <div className='site__navbar-logo'>
+    <img src={images.boreaslogo} alt="brand__logo" />
+    </div>
+    <ul className='site__navbar-links'>
+      <li className='link__opensans'><a href='#home'>Home</a></li>
+      <li className='link__opensans'><a href='#about'>About</a></li>
+      <li className='link__opensans'><a href='#menu'>Menu</a></li>
+      <li className='link__opensans'><a href='#awards'>Awards</a></li>
+      <li className='link__opensans'><a href='#contact'>Contact</a></li>
+    </ul>
+    <div className='site__navbar-login'>
+    <a href="login" className='link__opensans'>Log in / Register</a>
+    <div></div>
+    <a href="/" className='link__opensans'>Book a table</a>
+    </div>
+  </nav>
 );
 
 export default Navbar;
